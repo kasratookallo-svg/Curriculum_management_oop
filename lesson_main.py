58de3a4ed522896e7f5a6c00069326849020b7cc
Jalase10_git_kasra <kasratookallo@gmail.com>
2025-11-10 14:33:01 +0330

    Curriculum management has been designated by Object Oriented Management.

---
+++ lesson_main.py
@@ -0,0 +1,25 @@
+
+from lesson_module import Curriculum
+lesson_dict = {}
+lesson_code = None
+for i in range(2):
+    Lesson_code = int(input("Enter Lesson Code : "))
+    lesson_name = input("Enter Lesson Name : ")
+    teacher_name = input("Enter Teacher's Name : ")
+    lesson_code = None
+
+    #address = input("Enter Address : ")
+    #email = input("Enter Email : ")
+   #new_lesson = Curriculum((lesson_code, lesson_name, teacher_name) )
+
+  #  print("Saved.")
+   # print("-" * 15)
+   # print(new_lesson.__dict__)
+   # print("Saved.")
+    #print("-" * 15)
+
+
+#for lesson in study_list:
+ #   print(lesson)
+  #  print("Study Info = ",lesson.__dict__)
+
---
+++ lesson_module.py
@@ -0,0 +1,31 @@
+import re
+
+
+class   Curriculum:
+    #Donder _ Double Underlines or magic function
+    # Method
+    def __init__(self,lesson_name,lesson_code, teacher_name , lesson_credits):
+        # Property
+        self.lesson_name = lesson_name
+        self.lesson_code = lesson_code
+        self.teacher_name = teacher_name
+        self.lesson_credits = lesson_credits
+        self.Lesson_list = []
+
+    def add_lesson(self, lesson):
+        self.lesson_list.append(lesson)
+
+    # Method
+    def save(self):
+        print("INFO : " , self.lesson_name , self.lesson_code ,  self.teacher_name , self.lesson_credits)
+    # Method
+    def validation(self):
+        return (re.match(r"^[a-zA-Z\s]{3,30}$",self.lesson_name ) and
+                re.match(r"^[a-zA-Z\s]{3,30}$", self.teacher_name) and
+                self.lesson_code >= 0 and
+                20 >= self.lesson_credits >= 0)
+
+    def __repr__(self):
+        return f" {self.lesson_name:10} (Code) {self.lesson_code}: (Credits) {self.lesson_credits:10} - Teacher's Name : {self.teacher_name:10} "
+        #return "Person(name={},age={})".format(self.name, self.age)
+
